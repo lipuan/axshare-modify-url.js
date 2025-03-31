ae88f5b5e67d11a51108a767935ea850b61d4122
lipuan <lipuan@aliyun.com>
2025-03-31 11:26:32 +0800

    更新描述

--- axshare-modify-url.js
+++ axshare-modify-url.js
@@ -2,7 +2,7 @@
 // @name         axure share资源url修改
 // @namespace    http://tampermonkey.net/
 // @version      2025-03-28
-// @description  try to take over the world!
+// @description  该脚本解决了自行搭建的 axure cloud使用外网映射时非80端口会导致所有图片无法显示的问题。
 // @author       lipuan
 // @match        http://demo.xxxx1.cn:xxxx2/*/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=togeek.cn
